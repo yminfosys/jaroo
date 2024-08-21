8929bfdb042111509047a76cee81529328256c7b
Sukanta <sukanta@Sukantas-MBP.lan>
2024-08-21 11:18:27 +0530

    updates31aa

--- module/db/con.js
+++ module/db/con.js
@@ -5,7 +5,7 @@ const mongojs = require('mongojs');
 var dotenv=require('dotenv').config();
 
 
-const uri = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/jarooteam";
+const uri = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/jgfteam";
 
 const connectDB = async () => {
   try {
